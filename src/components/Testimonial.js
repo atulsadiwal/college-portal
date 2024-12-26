1bd5c2bd6916b26f8bf44d9153c0f14f6cfe0977
JatinBrandshow <jatin.yadav@brandshow.in>
2024-12-26 15:09:19 +0530

    Added demo images to Achievements and Testimonials

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -11,7 +11,7 @@ const testimonials = [
     category: 'Alumni',
     testimonial:
       '“The college transformed my career trajectory. The world-class faculty and vibrant community gave me opportunities I never imagined!”',
-    image: '/john-doe.jpg',
+    image: '/image/student1.png',
   },
   {
     name: 'Jane Smith',
@@ -19,7 +19,7 @@ const testimonials = [
     category: 'Students',
     testimonial:
       '“Being part of this college is an experience of a lifetime. The state-of-the-art infrastructure and mentorship are unmatched.”',
-    image: '/jane-smith.jpg',
+    image: '/image/student2.png',
   },
   {
     name: 'Emily Davis',
@@ -27,7 +27,7 @@ const testimonials = [
     category: 'Alumni',
     testimonial:
       '“This institution laid the foundation for my successful career. I owe everything to the incredible support I received here.”',
-    image: '/emily-davis.jpg',
+    image: '/image/student3.png',
   },
   {
     name: 'Michael Brown',
@@ -35,7 +35,7 @@ const testimonials = [
     category: 'Students',
     testimonial:
       '“As a parent, I couldn’t be happier. My son has gained skills and confidence that I couldn’t have imagined.”',
-    image: '/michael-brown.jpg',
+    image: '/image/student4.png',
   },
 ];
 
@@ -126,11 +126,10 @@ const Testimonial = () => {
           {['All', 'Alumni', 'Students'].map((category) => (
             <button
               key={category}
-              className={`px-4 py-2 rounded-full font-medium ${
-                categoryFilter === category
+              className={`px-4 py-2 rounded-full font-medium ${categoryFilter === category
                   ? 'bg-yellow-600 text-white'
                   : 'bg-white text-yellow-600 hover:bg-yellow-300'
-              }`}
+                }`}
               onClick={() => setCategoryFilter(category)}
             >
               {category}
